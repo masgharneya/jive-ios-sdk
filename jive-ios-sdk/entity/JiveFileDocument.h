dbf885237f59db95bdea35b9261ca6c935ac203e
Mohammad Asgharneya <masgharneya@gmail.com>
2015-09-30 17:43:33 -0400

    added JiveFileDocument for easier file upload support

---
+++ jive-ios-sdk/entity/JiveFileDocument.h
@@ -0,0 +1,13 @@
+//
+//  JiveFileDocument.h
+//  jive-ios-sdk
+//
+//  Created by Mohammad Asgharneya on 9/30/15.
+//  Copyright © 2015 Jive Software. All rights reserved.
+//
+
+#import "JiveDocument.h"
+
+@interface JiveFileDocument : JiveDocument
+
+@end
---
+++ jive-ios-sdk/entity/JiveFileDocument.m
@@ -0,0 +1,19 @@
+//
+//  JiveFileDocument.m
+//  jive-ios-sdk
+//
+//  Created by Mohammad Asgharneya on 9/30/15.
+//  Copyright © 2015 Jive Software. All rights reserved.
+//
+
+#import "JiveFileDocument.h"
+
+@implementation JiveFileDocument
+
+NSString * const JiveFileDocumentType = @"file";
+
+- (NSString *)type {
+    return JiveFileDocumentType;
+}
+
+@end
